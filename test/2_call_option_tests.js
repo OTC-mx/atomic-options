0580e8778332c805408e871d99aec97b6617d358
khelmy <khelmy@andrew.cmu.edu>
2019-09-28 12:05:49 -0400

    Add skeleton for new tests, unignore build

---
+++ test/2_call_option_tests.js
@@ -0,0 +1,12 @@
+const CallOption = artifacts.require("call_option");
+const OptionFactory = artifacts.require("option_factory");
+const TokenA = artifacts.require("TokenA");
+const TokenB = artifacts.require("TokenB");
+
+contract("1st CallOption test suite", async accounts => {
+
+  it("should ", async () => {
+
+  });
+
+});
